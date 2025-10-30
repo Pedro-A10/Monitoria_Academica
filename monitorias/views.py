841d5262e249b2c18c442a3149f3668ac0cd85fd
Pedro A. <pedroaugusto732@gmail.com>
2025-10-30 19:01:32 -0300

    Criação das views para o CRUD (Listar, deletar, criar e atualizar)

--- monitorias/views.py
+++ monitorias/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from models import Monitoria
+from forms import MonitoriaForm
 
-# Create your views here.
+def listar_monitorias(request):
+    monitorias = Monitoria.objects.all()
+    return render(request, 'monitorias/listar.html', {'monitorias': monitorias})
+
+def criar_monitoria(request):
+  if request.method == 'POST':
+    form = MonitoriaForm(request.POST)
+    if form.is_valid():
+      monitoria = form.save(commit=False)
+      monitoria.monitor = request.user
+      monitoria.save()
+      return redirect('listar_monitorias')
+    else:
+      form = MonitoriaForm()
+      return render(request, 'monitorias/form.html', {'form': form})
+
+def atualizar_monitoria(request):
+  momitoria = get_object_or_404(Monitoria, id=id)
+  form = MonitoriaForm(request.POST or None, instance=momitoria)
+  if form.is_valid():
+    form.save()
+    return redirect('listar_monitorias')
+  return render(request, 'monitorias/form.html', {'form': form})
+
+def excluir_monitoria(request):
+  monitoria = get_object_or_404(Monitoria, id=id)
+  if request.method == 'POST':
+    monitoria.delete()
+    return redirect('listar_monitorias')
+  return render(request, 'monitorias/deletar.html', {'form': MonitoriaForm()})
