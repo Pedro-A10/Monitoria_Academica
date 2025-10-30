c93a70f196dbe374a570590eefd696e5a5cb32e9
Pedro A. <pedroaugusto732@gmail.com>
2025-10-30 19:00:42 -0300

    criação do formulário com forms.py

---
+++ monitorias/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from models import Monitoria
+
+class MonitoriaForm(forms.ModelForm):
+  class Meta:
+    model = Monitoria
+    fields = [
+      'titulo',
+      'descricao',
+      'professor',]
