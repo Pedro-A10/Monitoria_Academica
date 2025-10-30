ffa45ae562ab26a2d9bf3322a1b1bb33b7d7c432
Pedro A. <pedroaugusto732@gmail.com>
2025-10-30 18:59:48 -0300

    Modificação no models.py de mentorias eatualização no admin.py

--- monitorias/models.py
+++ monitorias/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Monitoria(models.Model):
+  titulo = models.CharField(max_length=100)
+  descricao = models.TextField()
+  professor = models.CharField(max_length=100)
+  monitor = models.ForeignKey(User, on_delete=models.CASCADE)
+  data = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.titulo
+
